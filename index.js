6dd299f53b6143450adafeaf372c6b51642f281c
DominicDev <s0l0estudi4r@gmail.com>
2025-06-08 16:18:14 +0200

    v0.0.3

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Variables para obtener el dirname en ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const httpServer = createServer(app);
@@ -8,7 +14,7 @@ const io = new Server(httpServer);
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(import.meta.dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 function onConnection(socket) {
   socket.on('drawing', data => socket.broadcast.emit('drawing', data));
@@ -16,4 +22,4 @@ function onConnection(socket) {
 
 io.on('connection', onConnection);
 
-http.listen(port, () => console.log(`Open in http://localhost:${port}`));
+httpServer.listen(port, () => console.log(`Open in http://localhost:${port}`));
